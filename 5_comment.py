1842695235a5d09e7f79b6bf42654d25b326a833
N Aditiya <94776808+Lucifwr233@users.noreply.github.com>
2024-01-24 15:03:05 +0700

    Create 5_comment.py

---
+++ 5_comment.py
@@ -0,0 +1,15 @@
+# Variabel ini menyimpan nama 'Perseus Evans'
+name = 'Perseus Evans'
+
+"""
+Ini adalah Block Comment,
+Teks ini akan diabaikan oleh Python.
+"""
+
+'''
+Ini adalah Block Comment,
+Teks ini akan diabaikan oleh Python.
+'''
+
+print(name)
+print("Hello World!")
